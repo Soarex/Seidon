5f8eab4afecfa44e94e0418d8d920bc3cf030223
Alessio Loddo <alessio_98x@hotmail.it>
2021-09-18 12:13:25 +0200

    Added missing assets

--- Sandbox/src/HierarchyPanel.h
+++ Sandbox/src/HierarchyPanel.h
@@ -5,16 +5,4 @@ class HierarchyPanel
 {
 public:
 	HierarchyPanel();
-
-	void OnImGuiRender();
-
-	Seidon::Entity GetSelectedEntity() const { return selectedEntity; }
-	void SetSelectedEntity(Seidon::Entity entity);
-
-private:
-	void DrawEntityNode(Seidon::Entity entity);
-	void DrawComponents(Seidon::Entity entity);
-
-private:
-	Seidon::Entity selectedEntity;
 };
